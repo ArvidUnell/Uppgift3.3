4f7404ce5b7be52f1d3b8074f4ea32aca39f1f33
arun06001 <arun06001@E-PW0234KE>
2023-10-04 12:37:09 +0200

    Add project files.

---
+++ Uppgift3.3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Uppgift3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många timmar vill du hyra bil?");
+            byte timmar = byte.Parse(Console.ReadLine());
+
+            if (timmar*80 < 950)
+            {
+                Console.WriteLine($"Det kommer att kosta {timmar*80} kr.");
+            }
+            else
+            {
+                Console.WriteLine("Det kommer att kosta 950 kr.");
+            }
+
+            Console.WriteLine("Tryck på valfri knapp för att avsluta");
+            Console.ReadKey();
+        }
+    }
+}
